5cb14186de537375b02e89cc2dcc76b1b0dbe6c8
ywerling <ywerling@hotmail.de>
2024-08-12 08:05:35 +0200

    Include Bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-TBD
+Installation
+To get Bootstrap to work:
+    pip uninstall bootstrap_flask
+    pip uninstall flask_bootstrap
+    pip install bootstrap_flask
+
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
 
 #creates the flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = ('52jMEfBA3347dbefePSSiheXox3E7e')
+Bootstrap5(app)
 
 #start page of the webapplication
 @app.route("/")
