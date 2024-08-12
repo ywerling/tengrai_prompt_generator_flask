b043591f0c6e62a3e82eb0fad8024425b494a0a1
ywerling <ywerling@hotmail.de>
2024-08-12 07:35:07 +0200

    Create basic structure for Flask

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from flask import Flask, render_template
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#creates the flask instance
+app = Flask(__name__)
 
+#start page of the webapplication
+@app.route("/")
+def home():
+    return render_template('index.html')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#ensures that the application keeps running
+if __name__ == "__main__":
+    #remove the debug=True statement before deploment
+    app.run(debug=True)
