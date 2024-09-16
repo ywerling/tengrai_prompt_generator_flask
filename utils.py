6746e5325ead89e6b604134794a21d64faabcf60
ywerling <ywerling@hotmail.de>
2024-09-16 11:55:47 +0200

    Generate simple prompt for generic path

--- utils.py
+++ utils.py
@@ -1,13 +1,32 @@
+import prompt_parameters
+
 def process_generic_form_data(form_data):
     # Process form data from the request and return the necessary information
     subject = form_data.get("subject")
     background = form_data.get("background")
     style = form_data.get("style")
+
+    prompt = ""
+
+    if len(subject) >0 :
+        prompt+=subject
+        prompt+=", "
+
+    if len(background) > 0:
+        prompt += background
+        prompt += ", "
+
+    if style != prompt_parameters.NONE_STRING:
+        prompt += style
+        prompt += "."
+
+    return prompt
+
     # Continue processing all necessary fields
     # Return the processed data as needed
-    return {
-        'subject': subject,
-        'background': background,
-        'style': style,
-        # Add more fields here
-    }
+    # return {
+    #     'subject': subject,
+    #     'background': background,
+    #     'style': style,
+    #     # Add more fields here
+    # }
--- views/generic.py
+++ views/generic.py
@@ -9,25 +9,22 @@ generic_bp = Blueprint('generic', __name__, url_prefix='/generic')
 @generic_bp.route("/", methods=["GET", "POST"])
 def generic():
     form = GenericForm()
-    print("Generic called")
+    # print("Generic called")
     processed_result = ""
     if request.method == "POST":
     #     print("POST")
     # if form.validate_on_submit():
-        form_data = process_generic_form_data(request.form)
-        print(form_data)
+        prompt = process_generic_form_data(request.form)
         # subject = form.subject.data
-        background = form.background.data
-        style = form.style.data
-
-        subject = form_data['subject']
+        # background = form.background.data
+        # style = form.style.data
 
         # Process form data to create the prompt
         # Redirect or render the template with context as needed
         # Process the form data as needed (this is an example)
-        processed_result = f"Subject: {subject}, Background: {background}, Style: {style}"
-        print(processed_result)
-        return render_template('generic_refactored.html', form=form, result=processed_result)
+        # processed_result = f"Subject: {subject}, Background: {background}, Style: {style}"
+        print(prompt)
+        return render_template('generic_refactored.html', form=form, result=prompt)
 
     # Render the template without a result if the form is not submitted
     return render_template('generic_refactored.html', form=form)
