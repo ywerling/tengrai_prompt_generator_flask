4c8466528f9bb2b9f4aa0755fa763ba0163ed2ef
ywerling <ywerling@hotmail.de>
2024-09-16 16:30:56 +0200

    Generate prompts for landscape images

---
+++ views/landscape.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request
+from forms import LandscapeForm
+from utils import process_landscape_form_data
+import prompt_parameters
+
+landscape_bp = Blueprint('landscape', __name__, url_prefix='/landscape')
+
+
+@landscape_bp.route("/", methods=["GET", "POST"])
+def landscape():
+    form = LandscapeForm()
+
+    processed_result = ""
+    if request.method == "POST":
+
+    # Process form data to create the prompt
+        prompt = process_landscape_form_data(request.form)
+
+        print(prompt)
+        return render_template('landscape.html', form=form, prompt=prompt)
+
+    # Render the template without a result if the form is not submitted
+    return render_template('landscape.html', form=form)
