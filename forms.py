5a23a26e17ff685c82a4846b9cf99f94847700ca
ywerling <ywerling@hotmail.de>
2024-09-16 12:56:51 +0200

    Add all parameter types to the refactor generic prompt generator

--- forms.py
+++ forms.py
@@ -8,5 +8,12 @@ class GenericForm(FlaskForm):
     background = StringField("Background")
     style = SelectField("Style", choices=prompt_parameters.STYLE_LIST)
     art_type = SelectField("Art Type", choices=prompt_parameters.ART_TYPE_LIST)
-    # Additional fields as needed...
+    camera_angle = SelectField("Camera Angle", choices=prompt_parameters.CAMERA_ANGLE_LIST)
+    lighting = SelectField("Lighting", choices=prompt_parameters.LIGHTING_LIST)
+    color_palette = SelectField("Color Palette", choices=prompt_parameters.COLOR_PALETTE_LIST)
+    color_vibe = SelectField("Color Vibe", choices=prompt_parameters.COLOR_VIBES_LIST)
+    composition = SelectField("Composition", choices=prompt_parameters.COMPOSITIONS_LIST)
+    special_effect = SelectField("Special Effect", choices=prompt_parameters.SPECIAL_EFFECTS_LIST)
+    miscellaneous = SelectField("Miscellaneous", choices=prompt_parameters.MISCELLANEOUS_LIST)
+
     submit = SubmitField("Create Prompt")
--- utils.py
+++ utils.py
@@ -6,6 +6,13 @@ def process_generic_form_data(form_data):
     background = form_data.get("background")
     style = form_data.get("style")
     art_type = form_data.get("art_type")
+    camera_angle = form_data.get("camera_angle")
+    lighting = form_data.get("lighting")
+    color_palette = form_data.get("color_palette")
+    color_vibe = form_data.get("color_vibe")
+    composition = form_data.get("composition")
+    special_effect = form_data.get("special_effect")
+    miscellaneous = form_data.get("miscellaneous")
 
     prompt = ""
 
@@ -23,6 +30,34 @@ def process_generic_form_data(form_data):
 
     if art_type != prompt_parameters.NONE_STRING:
         prompt += art_type
+        prompt += ", "
+
+    if camera_angle != prompt_parameters.NONE_STRING:
+        prompt += camera_angle
+        prompt += ", "
+
+    if lighting != prompt_parameters.NONE_STRING:
+        prompt += lighting
+        prompt += ", "
+
+    if color_palette != prompt_parameters.NONE_STRING:
+        prompt += color_palette
+        prompt += ", "
+
+    if color_vibe != prompt_parameters.NONE_STRING:
+        prompt += color_vibe
+        prompt += ", "
+
+    if composition != prompt_parameters.NONE_STRING:
+        prompt += composition
+        prompt += ", "
+
+    if special_effect != prompt_parameters.NONE_STRING:
+        prompt += special_effect
+        prompt += ", "
+
+    if miscellaneous != prompt_parameters.NONE_STRING:
+        prompt += miscellaneous
         prompt += ", "
 
     return prompt
